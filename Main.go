7abcbb431387e88a32997481cff309c18b2ce0ba
aeonincode <khashayar.sasannia@hotmail.com>
2021-06-24 16:41:24 +0200

    first commit

---
+++ Main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go!")
+}
+
+// go run src/github.com/aeonincode/firstapp/Main.go
+// go build github.com/aeonincode/firstapp
+// go install github.com/aeonincode/firstapp
